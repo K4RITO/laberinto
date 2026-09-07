4fa09f08b9edc36d52628ca944f9b6b6c84fc36b
Cbas79ft <ioseft79@gmail.com>
2026-09-07 10:33:28 -0300

    Subiendo el código inicial

---
+++ pantalla.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from menu import menu_principal
+
+ventana = tk.Tk()
+ventana.title("Laberinto")
+ventana.geometry("800x600")
+
+frame = tk.Frame(ventana)
+frame.pack(expand=True)
+
+menu_principal(ventana, frame)
+
+ventana.mainloop()
